6538f3b91bb4df547023cecf65986e835559ca61
Neil Sawhney <Neil@neil-sawhney.com>
2021-12-29 14:50:28 -0500

    bloop

--- .bash_aliases
+++
@@ -1,1 +0,0 @@
-alias oni2="/home/neil/linuxInit/oni2/Onivim2-*.AppImage"
--- homeDirectoryFiles/setup.sh
+++ homeDirectoryFiles/setup.sh
@@ -7,3 +7,7 @@ ssh-add ~/.ssh/id_ed25519
 git submodule update --init --recursive
 cp -r vimFiles/.vim vimFiles/.vimrc ~/
 
+git config --global user.email "Neil@neil-sawhney.com"
+git config --global user.name "Neil Sawhney"
+
+
