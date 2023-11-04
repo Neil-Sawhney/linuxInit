8a73a6c9ddf395b3ea835fb841d284f7c7034154
Neil Sawhney <Neil@neil-sawhney.com>
2023-11-04 01:41:32 -0400

    auto switch theme:

--- homeDirectoryFiles/setup.sh
+++ homeDirectoryFiles/setup.sh
@@ -21,6 +21,7 @@ sudo apt install git-core curl fonts-powerline -y
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+sed -i 's/ZSH_THEME=\"robbyrussell\"/\#ZSH_THEME=\"robbyrussell\"\nZSH_THEME=\"powerlevel10k\/powerlevel10k\"/g' ~/.zshrc
 
 echo "bindkey -v" >> ~/.zshrc
 sudo reboot now
